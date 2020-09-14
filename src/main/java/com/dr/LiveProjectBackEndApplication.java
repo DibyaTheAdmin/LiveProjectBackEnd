0e2875376446923e17076056078976c9098a8b3b
DELL <DELL@ADMINRG-1EJELKO>
2020-09-14 13:48:40 +0530

    1st push of the project(1st).....

---
+++ src/main/java/com/dr/LiveProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.dr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveProjectBackEndApplication.class, args);
+	}
+
+}
